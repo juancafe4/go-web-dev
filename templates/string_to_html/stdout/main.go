2b3acbd373cccab46d69367c05a78faef18a1dcd
Juan Carlos <juancafe2@gmail.com>
2017-03-06 23:49:38 -0400

    Parsing strings

--- templates/string_to_html/stdout/main.go
+++ templates/string_to_html/stdout/main.go
@@ -1,21 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
 func main() {
-	name := "Juan Carlos Ferrel"
+	tpl, err := template.ParseFiles("tpl.gohtml")
 
-	tpl := `
-  <DOCTYPE html>
-  <html lang="eng"
-  <head>
-  <meta charset="UTF-8"
-  <title>Hello World!</title>
-  </head>
-  <body>
-  <h1>` + name + `</h1>
-  </body>
-  </html>
-  `
-	fmt.Println(tpl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
+
+// func main() {
+// 	name := "Juan Carlos Ferrel"
+
+// 	tpl := `
+//   <DOCTYPE html>
+//   <html lang="eng"
+//   <head>
+//   <meta charset="UTF-8"
+//   <title>Hello World!</title>
+//   </head>
+//   <body>
+//   <h1>` + name + `</h1>
+//   </body>
+//   </html>
+//   `
+// 	fmt.Println(tpl)
+// }
